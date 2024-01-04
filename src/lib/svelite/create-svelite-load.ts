55e7e0574192adb366b960fd8bec795356f40a60
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-01-04 18:23:12 +0430

    cleanup load

--- src/lib/svelite/create-svelite-load.ts
+++ src/lib/svelite/create-svelite-load.ts
@@ -1,10 +1,8 @@
 import type { PageModule } from '.';
 
 function matchRoute(url, pages) {
-	console.log(pages.map((x) => x.slug));
 	let result: any = {};
 	for (let page of pages) {
-		console.log('processing...', url, page.slug);
 
 		if (url === page.slug)
 			return {
@@ -19,14 +17,12 @@ function matchRoute(url, pages) {
 		if (!urlSplitted.length === slugSplitted) return {};
 
 		let params: any = {};
-		console.log('check', urlSplitted, slugSplitted);
 		for (let index in slugSplitted) {
 			if (urlSplitted[index] !== slugSplitted[index]) {
 				// check if slugSplitted is dynamic
 				if (slugSplitted[index]?.startsWith('{...')) {
 					result.page = page;
 
-					console.log('urlSplitted', urlSplitted.slice(index), index, urlSplitted);
 					params[slugSplitted[index].slice(4, slugSplitted[index].length - 1)] = urlSplitted
 						.slice(index)
 						.join('/');
@@ -49,72 +45,45 @@ function matchRoute(url, pages) {
 		}
 
 		if (result.page) return result;
-		console.log('after loop', result);
 	}
 	return result;
 }
 
 export function createSveliteLoad(api, pages, modules, layouts) {
 	return async (slug: string) => {
-		const dbpages = await api
-			.db('pages')
-			.find()
-			.all()
-			.then((res) => res.data);
+		const route = matchRoute(slug, pages);
 
-		//const allPages = [...pages, ...dbpages];
+		if (!route.page) return {};
+		const { page, params } = route;
+    
 
-		const matchedRoute = matchRoute(slug, pages);
-
-		console.log('matchedRoute: ', slug, matchedRoute);
-
-		if (!matchedRoute.page) return {};
-		const page = matchedRoute.page;
-
-		let layoutData = {};
+        // layout
 		if (page.layout) {
+            // layout component
 			page.layout.component = layouts[page.layout.name].component;
+
+            // layout load
 			if (layouts[page.layout.name].load) {
-				// layout Load
 				page.layout.props ??= {};
 				page.layout.props.data = await layouts[page.layout.name].load(
 					page.layout.props,
 					api,
-					matchedRoute.params
+					params
 				);
 			}
 		}
 
+        // Page (recursive)
 		async function initializeModule(module: PageModule) {
+            // page component
 			module.component = modules[module.name].component;
 
+            // page load
 			if (modules[module.name].load) {
-				module.props.data = await modules[module.name].load(module.props, api, matchedRoute.params);
-
-				console.log('DATA: ', module.name, module.props.data);
-				// reload page after running action
-				for (let key in module.props.data) {
-					if (typeof module.props.data[key] === 'function') {
-						const fn = module.props.data[key];
-						if (fn.constructor.name === 'AsyncFunction') {
-							module.props.data[key] = async (...args) => {
-								const result = await fn(...args);
-
-								location.reload();
-								return result;
-							};
-						} else {
-							module.props.data[key] = (...args) => {
-								const result = fn(...args);
-								location.reload();
-
-								return result;
-							};
-						}
-					}
-				}
+				module.props.data = await modules[module.name].load(module.props, api, params);
 			}
 
+            // initialize all modules of page recursively
 			for (let key in module.props) {
 				let prop = module.props[key];
 				if (
@@ -129,6 +98,8 @@ export function createSveliteLoad(api, pages, modules, layouts) {
 				}
 			}
 		}
+        
+        // call initialize module for all modules of page
 		for (let module of page.modules) {
 			await initializeModule(module);
 		}

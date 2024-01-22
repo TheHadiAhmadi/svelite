ee0badca47e049431408b7cd7aa7ffda3f24f549
Hadi Ahmadi <TheHadiAhmadi@gmail.com>
2024-01-22 18:35:03 +0430

    fix

--- vite.build.config.js
+++ vite.build.config.js
@@ -4,13 +4,13 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default {
   plugins: [svelte()],
   build: {
+	  ssr: true,
     rollupOptions: {
       input: {
         svelite: path.resolve("./src/lib/svelite"),
         client: path.resolve("./src/lib/client"),
         server: path.resolve("./src/lib/server"),
         vite: path.resolve("./src/lib/vite"),
-        components: path.resolve("./src/lib/components/index"),
       },
     },
   },

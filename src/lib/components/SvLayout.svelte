24bac78c7df2ce7402480c702bfe2ec055e04d5e
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-02-21 20:46:32 +0430

    update

--- src/lib/components/SvLayout.svelte
+++ src/lib/components/SvLayout.svelte
@@ -1,9 +1,11 @@
 <script>
     import SvPage from './SvPage.svelte'
 	let { page } = $props();
+    
+    console.log(page)
 </script>
 
-{#if page.layout}
+{#if page.layout?.component}
 	<svelte:component this={page.layout.component} {...page.layout.props}>
         <SvPage {page}/>
 	</svelte:component>
